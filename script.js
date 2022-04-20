bd633d10cfdb93ad116625d790e84c47c3832c14
jndrssn <99423543+jndrssn@users.noreply.github.com>
2022-04-20 09:39:01 -0300

    Porfolio
    Proyecto integrador para Argentina Programa

---
+++ script.js
@@ -0,0 +1,5 @@
+function cambiar_parrafo(){
+    document.getElementById("edit-acercade").style.display="block";
+    let texto = document.getElementById("text-acercade").innerText;
+    console.log(texto);
+};
