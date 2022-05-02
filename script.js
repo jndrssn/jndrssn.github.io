497e22fe2214608960ced921a7c89266fc85642c
jndrssn <99423543+jndrssn@users.noreply.github.com>
2022-05-02 14:16:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function cambiar_parrafo(){
-    document.getElementById("edit-acercade").style.display="block";
+function cambiar_parrafo() {
+    document.getElementById("edit-acercade").style.display ="block";
     let texto = document.getElementById("text-acercade").innerText;
     console.log(texto);
-};
+  };
+
+  function myFunction2(valor) {
+    document.getElementById("text-acercade").innerText = valor;
+  };
+
+  function logMessage(message) {
+    console.log(message + "<br>");
+  }
+
+  let textarea = document.getElementById("edit-acercade")
+  textarea.addEventListener('keyup', (e) => {
+    logMessage('Key "${e.key}" released [event: keyup]');
+    if (e.key=="Enter") {
+      document.getElementById("edit-acercade").style.display="none"
+    }
+  });
+
+  
